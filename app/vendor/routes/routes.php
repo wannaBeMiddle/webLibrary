21326cffb53e49605dfa7624b3c7dc790894e594
Evgeniy Mukhin <muhin@intervolga.ru>
2022-05-10 15:54:06 +0300

    init commit

--- app/vendor/routes/routes.php
+++ app/vendor/routes/routes.php
@@ -4,50 +4,5 @@ return [
 		'controller' => 'main',
 		'action' => 'index',
 		'notAuthAccess' => true
-	],
-	'/basket/' => [
-		'controller' => 'basket',
-		'action' => 'index',
-		'notAuthAccess' => false
-	],
-	'/basket/{int}' => [
-		'controller' => 'basket',
-		'action' => 'setProduct',
-		'notAuthAccess' => false
-	],
-	'/basket/delete/{int}' => [
-		'controller' => 'basket',
-		'action' => 'delete',
-		'notAuthAccess' => false
-	],
-	'/basket/pay/{int}' => [
-		'controller' => 'basket',
-		'action' => 'pay',
-		'notAuthAccess' => false
-	],
-	'/personal/' => [
-		'controller' => 'personal',
-		'action' => 'index',
-		'notAuthAccess' => false
-	],
-	'/signin/' => [
-		'controller' => 'signin',
-		'action' => 'index',
-		'notAuthAccess' => true
-	],
-	'/signin/doAuth' => [
-		'controller' => 'signin',
-		'action' => 'doAuth',
-		'notAuthAccess' => true
-	],
-	'/signup/' => [
-		'controller' => 'signup',
-		'action' => 'index',
-		'notAuthAccess' => true
-	],
-	'/signup/doReg' => [
-		'controller' => 'signup',
-		'action' => 'doReg',
-		'notAuthAccess' => true
-	],
+	]
 ];
